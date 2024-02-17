286e0f8ea16362c8266fd823d746ca4e1de889f2
kazvee <kazveedev@gmail.com>
2024-02-17 15:19:22 -0700

    feat: implement nextVersion function

--- index.js
+++ index.js
@@ -1,7 +1,23 @@
-const sayHello = () => {
-  return 'Hello World! 🌎';
+/**
+ * Increments the software version number
+ *
+ * @param {string} currentVersion - The current version string
+ * @returns {string} - The incremented new version string
+ */
+const nextVersion = (currentVersion) => {
+  const removeDots = currentVersion.replace(/\./g, '');
+  const dotCount = currentVersion.length - removeDots.length;
+  const incrementVersionNumber = String(Number(removeDots) + 1);
+
+  let newVersion = '';
+  for (let i = incrementVersionNumber.length - 1, dotAdded = 0; i >= 0; i--) {
+    newVersion = incrementVersionNumber[i] + newVersion;
+    if (dotAdded < dotCount && i > 0) {
+      newVersion = '.' + newVersion;
+      dotAdded++;
+    }
+  }
+  return newVersion;
 };
 
-console.log(sayHello());
-
-module.exports = sayHello;
+module.exports = nextVersion;
