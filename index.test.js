c47f98b35fe29ff9af73636a0660aab02ab22d62
kazvee <kazveedev@gmail.com>
2024-02-17 15:18:26 -0700

    test: add tests for nextVersion

--- index.test.js
+++ index.test.js
@@ -1,5 +1,31 @@
-const sayHello = require('./index');
+const nextVersion = require('./index');
 
-test('returns "Hello World! 🌎"', () => {
-  expect(sayHello()).toBe('Hello World! 🌎');
+it('Returns next version number as a string', () => {
+  const result = nextVersion('1');
+  expect(typeof result).toBe('string');
+});
+
+it('Increments version correctly', () => {
+  const result = nextVersion('1.2.3');
+  expect(result).toBe('1.2.4');
+});
+
+it('Increments correctly when current version section is at 9', () => {
+  const result = nextVersion('0.9.9');
+  expect(result).toBe('1.0.0');
+});
+
+it('Handles whole numbers', () => {
+  const result = nextVersion('1');
+  expect(result).toBe('2');
+});
+
+it('Handles many dots in the version', () => {
+  const result = nextVersion('1.2.3.4.5.6.7.8');
+  expect(result).toBe('1.2.3.4.5.6.7.9');
+});
+
+it('Maintains the same number of decimal points', () => {
+  const result = nextVersion('9.9');
+  expect(result).toBe('10.0');
 });
